1a85616c1fa2349338c511bacc2ea19f79267713
Josh Ethridge <joshua.ethridge13@gmail.com>
2020-05-11 22:09:17 -0400

    Add cards for each game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ACVR';
+  games = [
+  {
+    title: 'Animal Crossing',
+    year: 2001,
+    boxArt: '../assets/ac.jpg'
+  },
+  {
+    title: 'Wild World',
+    year: 2005,
+    boxArt: '../assets/ww.jpg'
+  },
+  {
+    title: 'City Folk',
+    year: 2008,
+    boxArt: '../assets/cf.jpg'
+  },
+  {
+    title: 'New Leaf',
+    year: 2012,
+    boxArt: '../assets/nl.jpg'
+  },
+  {
+    title: 'New Horizons',
+    year: 2020,
+    boxArt: '../assets/nh.jpg'
+  }];
 }
